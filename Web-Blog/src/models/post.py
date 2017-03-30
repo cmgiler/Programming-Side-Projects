e4de604032dc1e66784960164aa13a617c026234
cmgiler <cmgiler@gmail.com>
2017-03-29 20:36:37 -0400

    Finish Blog Web-App Project (Basic web-based blog functions)

---
+++ Web-Blog/src/models/post.py
@@ -0,0 +1,47 @@
+import datetime
+import uuid
+
+from src.common.database import Database
+
+__author__ = 'cmgiler'
+
+class Post(object):
+
+    def __init__(self, blog_id, title, content, author, created_date=datetime.datetime.utcnow(), _id=None):
+        self.blog_id = blog_id
+        self.title = title
+        self.content = content
+        self.author = author
+        self.created_date = created_date
+
+        # Random uuid (universally unique identifier)
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    def save_to_mongo(self):
+        Database.insert(collection = 'posts',
+                        data = self.json())
+
+    def json(self):
+        return {
+            '_id': self._id,
+            'blog_id': self.blog_id,
+            'author': self.author,
+            'content': self.content,
+            'title': self.title,
+            'created_date': self.created_date
+        }
+
+    @classmethod
+    def from_mongo(cls, id):
+        # ex: Post.from_mongo('123')
+        post_data = Database.find_one(collection='posts',
+                                      query={'_id': id})
+
+        # Return an object
+        return cls(**post_data)
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in \
+                Database.find(collection='posts',
+                              query={'blog_id': id})]
